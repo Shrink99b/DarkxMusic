6ffdd8f66842283bdf4df6ba2b46885a5a6b13de
PRINCE ARIYAN <115484197+Prince-ariyan-143@users.noreply.github.com>
2022-10-19 16:47:51 +0600

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -55,7 +55,7 @@ async def skip(_, message: Message):
     for x in callsmusic.pytgcalls.active_calls:
         ACTV_CALLS.append(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
-        await message.reply_text("Nothing is playing what to skip 🥲")
+        await message.reply_text("ɴᴏᴛʜɪɴɢ ɪs ᴘʟᴀʏɪɴɢ ᴡʜᴀᴛ ᴛᴏ sᴋɪᴘ 🥲")
     else:
         queues.task_done(chat_id)
         

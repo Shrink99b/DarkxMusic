1c3c813234b5e218259da7d3bbc148e25aeaf70e
PRINCE ARIYAN <115484197+Prince-ariyan-143@users.noreply.github.com>
2022-10-19 16:52:01 +0600

    Update assistant.py

--- handlers/assistant.py
+++ handlers/assistant.py
@@ -23,7 +23,7 @@ async def join_chat(c: Client, m: Message):
             (await user.get_me()).id,
             can_manage_voice_chats=True
         )
-        return await user.send_message(chat_id, "Assistant successfully joined the chat.​")
+        return await user.send_message(chat_id, "ᴀssɪsᴛᴀɴᴛ sᴜᴄᴄᴇssғᴜʟʟʏ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴄʜᴀᴛ​")
     except UserAlreadyParticipant:
         admin = await m.chat.get_member((await user.get_me()).id)
         if not admin.can_manage_voice_chats:
@@ -31,5 +31,5 @@ async def join_chat(c: Client, m: Message):
                 (await user.get_me()).id,
                 can_manage_voice_chats=True
             )
-            return await user.send_message(chat_id, "Assistant already joined the chat.​")
-        return await user.send_message(chat_id, "Assistant already joined the chat lodu.​")
+            return await user.send_message(chat_id, "ᴀssɪsᴛᴀɴᴛ sᴜᴄᴄᴇssғᴜʟʟʏ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴄʜᴀᴛ​")
+        return await user.send_message(chat_id, "ᴀssɪsᴛᴀɴᴛ sᴜᴄᴄᴇssғᴜʟʟʏ ᴊᴏɪɴᴇᴅ ᴛʜᴇ ᴄʜᴀᴛ")
